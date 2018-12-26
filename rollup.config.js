80b281b5d585e362f4b3337d267307b9e0d7001f
ray851107 <16625236+ray851107@users.noreply.github.com>
2018-12-27 00:52:37 +0800

    remove rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
 import buble from 'rollup-plugin-buble'
 
 export default {
   input: 'lib/main.js',
-  plugins: [nodeResolve(), buble({ objectAssign: true })],
+  plugins: [buble({ objectAssign: true })],
   output: {
     format: 'iife',
     file: 'parallax-background-image.js',

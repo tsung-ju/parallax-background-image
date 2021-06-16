d14f1e7183a2b485aff6a6e72790b3f943dc4ff6
tsung-ju <71379180+tsung-ju@users.noreply.github.com>
2021-06-16 19:19:11 +0800

    Support non-positive velocity

--- src/transform.js
+++ src/transform.js
@@ -13,7 +13,8 @@ export function createTransform(image, element, viewport, options) {
 
   const centerX = 0.5 * (viewport.w - element.w);
   const centerY = 0.5 * (viewport.h - element.h);
-  const v = backend === "3d" ? velocity.y : 1;
+  let v = backend === "3d" ? velocity.y : 1;
+  if (v <= 0) v = 1;
 
   x = (x - centerX * (velocity.x - 1)) / v + element.x * (velocity.x / v - 1);
   y = (y - centerY * (velocity.y - 1)) / v + element.y * (velocity.y / v - 1);
